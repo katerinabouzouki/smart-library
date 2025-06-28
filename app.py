2467f8351feea421c03ecd6ea71deeb767dab7aa
Katerina Bouzouki <katerinampouzouki@gmail.com>
2025-06-28 19:45:46 +0300

    Initial commit

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from datetime import date
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 # Ρυθμίσεις σύνδεσης MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'       # άλλαξε το αν χρειάζεται
-app.config['MYSQL_PASSWORD'] = 'Kate1234_.'         # βάλε το password σου
-app.config['MYSQL_DB'] = 'library_system'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
