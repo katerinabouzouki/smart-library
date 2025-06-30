ad2e944e326d1da40924a1698adc7b3e1978f5d0
Katerina Bouzouki <katerinampouzouki@gmail.com>
2025-06-30 16:27:32 +0300

    Update app.py with latest changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# Ρυθμίσεις σύνδεσης MySQL
+# Login settings for MySQL
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
